04b061464cc3c62d5b1294dedb42333ba420a6bd
briyu <briyu73@gmail.com>
2025-09-09 20:16:10 +0930

    baseentity extensions

---
+++ Test/BaseEntityExtensions.cs
@@ -0,0 +1,132 @@
+﻿
+using BAM.Libraries.SharedEDMLib.Model.Structural;
+
+namespace BAM.Libraries.SharedEDMLib.Extensions;
+
+public static class BaseEntityExtensions
+{
+	// *******************************************************************************************
+	// Members
+	// *******************************************************************************************
+
+	#region Members
+
+	#endregion Members
+
+	// *******************************************************************************************
+	// Properties
+	// *******************************************************************************************
+
+	#region Properties
+
+	#endregion
+
+	// *******************************************************************************************
+	// Commands
+	// *******************************************************************************************
+
+	#region Commands
+
+	#endregion
+
+	// *******************************************************************************************
+	// Public Methods
+	// *******************************************************************************************
+
+	#region Public Methods
+
+	// Flattens and returns all child BaseEntity objects within the passed node
+	public static IEnumerable<BaseEntity> Flatten(this BaseEntity node)
+	{
+		var list = new List<BaseEntity>();
+		list.Add(node);
+
+		var properties = node.GetType().GetProperties();
+		foreach (var property in properties) 
+		{
+			var value = property.GetValue(node) as BaseEntity;
+			if (value != null)
+			{
+				list.AddRange(value.Flatten());
+			}
+		}
+
+		return list;
+	}
+
+	// returns a name property if found on the object, otherwise it returns it's id
+	public static string GetName(this BaseEntity node)
+	{
+		var nameProperty = node.GetType().GetProperty("Name");
+		if (nameProperty != null)
+		{
+			return nameProperty.GetValue(node) as string ?? string.Empty;
+		}
+
+		return node.Id.ToString();
+	}
+
+	public static bool TryGetObjectName(this BaseEntity baseEntity, out string name)
+	{
+		var nameProperty = baseEntity.GetType().GetProperties()
+			.Where(p => p.CanRead)
+			.FirstOrDefault(p => p.Name == "Name");
+
+		if (nameProperty != null)
+		{
+			name = nameProperty.GetValue(baseEntity)?.ToString() ?? string.Empty;
+			return true;
+		}
+
+		name = string.Empty;
+		return false;
+	}
+
+	public static bool TryGetObjectName(this BaseEntity baseEntity, string name)
+	{
+		// if object has a writeable name proeprty just use that
+		var nameProperty = baseEntity.GetType().GetProperties()
+			.Where(p => p.CanWrite)
+			.FirstOrDefault(p => p.Name == "Name");
+
+		if (nameProperty != null)
+		{
+			// check for an invalid empty or white spaced name
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				name = HelperFns.DEFAULT_UNKNOWN_NAME;
+			}
+
+			nameProperty.SetValue(baseEntity, name);
+			return true;
+		}
+
+		return false;
+	}
+
+	#endregion
+
+	// *******************************************************************************************
+	// Protected Methods
+	// *******************************************************************************************
+
+	#region Protected Methods
+
+	#endregion
+
+	// *******************************************************************************************
+	// Private Methods
+	// *******************************************************************************************
+
+	#region Private Methods
+
+	#endregion
+
+	// *******************************************************************************************
+	// Disposal Support
+	// *******************************************************************************************
+
+	#region IDisposalSupport
+
+	#endregion
+}
---
+++ Test/EagerLoadExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BAM.Libraries.SharedEDMLib.Extensions;
+
+public static class EagerLoadExtensions
+{
+	/// <summary>
+	/// Eagerly loads all navigation properties and collections for the specified entity type.
+	/// </summary>
+	/// <typeparam name="TEntity">The entity type to load.</typeparam>
+	/// <param name="query">The IQueryable to apply the includes to.</param>
+	/// <param name="dbContext">The DbContext instance to get metadata from.</param>
+	/// <returns>An IQueryable with all navigation properties included.</returns>
+	public static IQueryable<TEntity> IncludeAllNavigationProperties<TEntity>(
+			this IQueryable<TEntity> query,
+			DbContext dbContext)
+			where TEntity : class
+	{
+		if (query == null)
+			throw new ArgumentNullException(nameof(query));
+		if (dbContext == null)
+			throw new ArgumentNullException(nameof(dbContext));
+
+		var entityType = dbContext.Model.FindEntityType(typeof(TEntity));
+		if (entityType == null)
+			throw new InvalidOperationException($"Entity type {typeof(TEntity).Name} not found in the model.");
+
+		foreach (var navigation in entityType.GetNavigations())
+		{
+			query = query.Include(navigation.Name);
+		}
+
+		return query;
+	}
+
+	/// <summary>
+	/// Eagerly loads all navigation properties and collections recursively for the specified entity type.
+	/// </summary>
+	/// <typeparam name="TEntity">The entity type to load.</typeparam>
+	/// <param name="query">The IQueryable to apply the includes to.</param>
+	/// <param name="dbContext">The DbContext instance to get metadata from.</param>
+	/// <param name="maxDepth">The maximum depth for recursive loading (default is 3).</param>
+	/// <returns>An IQueryable with all navigation properties included recursively.</returns>
+	public static IQueryable<TEntity> IncludeAllNavigationPropertiesRecursive<TEntity>(
+			this IQueryable<TEntity> query,
+			DbContext dbContext,
+			int maxDepth = 3)
+			where TEntity : class
+	{
+		if (query == null)
+			throw new ArgumentNullException(nameof(query));
+		if (dbContext == null)
+			throw new ArgumentNullException(nameof(dbContext));
+		if (maxDepth < 0)
+			throw new ArgumentException("Max depth cannot be negative.", nameof(maxDepth));
+
+		return IncludeNavigationPropertiesRecursiveInternal(query, dbContext, typeof(TEntity), maxDepth);
+	}
+
+	private static IQueryable<TEntity> IncludeNavigationPropertiesRecursiveInternal<TEntity>(
+			IQueryable<TEntity> query,
+			DbContext dbContext,
+			Type entityType,
+			int maxDepth,
+			string currentPath = "")
+			where TEntity : class
+	{
+		if (maxDepth <= 0)
+			return query;
+
+		var entityMetadata = dbContext.Model.FindEntityType(entityType);
+		if (entityMetadata == null)
+			return query;
+
+		foreach (var navigation in entityMetadata.GetNavigations())
+		{
+			var navigationPath = string.IsNullOrEmpty(currentPath) ? navigation.Name : $"{currentPath}.{navigation.Name}";
+			query = query.Include(navigationPath);
+
+			// Recursively include navigation properties for the target type
+			var targetType = navigation.TargetEntityType.ClrType;
+			query = IncludeNavigationPropertiesRecursiveInternal(query, dbContext, targetType, maxDepth - 1, navigationPath);
+		}
+
+		return query;
+	}
+}
